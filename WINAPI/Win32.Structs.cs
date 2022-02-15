fa30bcf7ec276b9c67bd5e353cf0953344ae3e63
Wings <pgw00k@163.com>
2022-02-15 09:56:43 +0800

    [Init]

---
+++ WINAPI/Win32.Structs.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WINAPI
+{
+	#region RECT
+	[StructLayout(LayoutKind.Sequential)]
+	public struct Rect
+	{
+		public int left;
+		public int top;
+		public int right;
+		public int bottom;
+
+        public override string ToString()
+        {
+			string r = $"({top},{bottom},{left},{right})";
+			return r;
+        }
+    }
+	#endregion
+}
---
+++ WINAPI/Win32.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+/*
+ * 
+ * c
+ * 
+ */
+namespace WINAPI
+{
+
+
+    public class Win32
+    {
+
+        #region User32.dll
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hWndParent, IntPtr hWndChildAfter, string lpClass, string lpWindow);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool PrintWindow(IntPtr hwnd, IntPtr hdc, uint nFlags);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, IntPtr lParam);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, WinMsg msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        #endregion
+
+    }
+}
